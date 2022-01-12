1c875e36c51cbba38a214c5acda5b0d6bcee209a
dalton-b <daltonbassett@gmail.com>
2022-01-12 12:58:30 -0500

    concordance, nemo

---
+++ app/concordance_app.py
@@ -0,0 +1,9 @@
+import nltk
+
+nltk.app.concordance_app.app()
+
+# A concordance view shows us every occurrence of a given word, together with some context
+#
+# https://www.nltk.org/howto/concordance.html
+
+# https://www.nltk.org/api/nltk.app.concordance_app.html
---
+++ app/nemo_app.py
@@ -0,0 +1,8 @@
+import nltk
+
+nltk.app.nemo_app.app()
+
+# Seems to use regular expressions to find and replace the word "nemo" with
+# "MAKI" in the Lorem ipsum... text
+
+# https://www.nltk.org/api/nltk.app.nemo_app.html
