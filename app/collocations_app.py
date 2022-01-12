88e7bad2feefe2bc02aababa2779f869e6bcd880
dalton-b <daltonbassett@gmail.com>
2022-01-12 12:48:07 -0500

    Collocations

---
+++ app/collocations_app.py
@@ -0,0 +1,12 @@
+import nltk
+
+nltk.app.collocations_app.app()
+
+# Collocation is a group of two or more words that like to hang out together. 
+# Here are some examples of common collocations that you might know:
+# make tea - I made a cup of tea for lunch.
+# do homework - I did all of my homework yesterday. 
+#
+# Source: https://www.thoughtco.com/what-is-collocation-1211244
+
+# https://www.nltk.org/api/nltk.app.collocations_app.html
